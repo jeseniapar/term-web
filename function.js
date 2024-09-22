bd42f4747cb97c0c55b5e448653d67c5c41d28d6
Jesenia P <jeseniapartha6@gmail.com>
2024-09-22 13:44:40 -0500

    Update function.js

--- function.js
+++ function.js
@@ -16,6 +16,7 @@ function handleCommand(inputValue) {
     } else {
         const errorDisplay = document.createElement("p");
         errorDisplay.textContent = "stat not found... try one from above.";
+        errorDisplay.style.color = "#861a03";
         outputDiv.appendChild(errorDisplay);
     }
 
@@ -25,22 +26,34 @@ function handleCommand(inputValue) {
     moveInputBelow();
 }
 
+
 function moveInputBelow() {
     const container = document.getElementById("output-container");
     const inputWrapper = document.getElementById("input-wrapper");
-
     const visibleInput = document.getElementById("visible-input");
-    visibleInput.textContent = '';
-    
-    container.appendChild(inputWrapper);
-
     const input = document.getElementById("myInput");
+
+    visibleInput.textContent = '';
+    container.appendChild(inputWrapper);
     input.value = '';
-    
+    input.focus(); 
     window.scrollTo(0, document.body.scrollHeight);
 }
+
 
 function updateVisibleInput(event) {
     const visibleInput = document.getElementById("visible-input");
     visibleInput.textContent = event.target.value;
 }
+
+window.onload = function() {
+    const input = document.getElementById("myInput");
+    input.focus(); 
+    input.addEventListener("input", updateVisibleInput);
+    input.addEventListener("keypress", function(event) {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            handleCommand(input.value);
+        }
+    });
+};
